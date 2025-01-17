ae69dce5ab79b493b1369c08b775d437f3e797fc
Faizan Baig <faizan.baig@infinityplus1.in>
2025-01-17 15:40:29 +0530

    Added route

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\Middleware;
+
 
 class AdminController extends Controller
 {
-    //
+    
+    public function __construct()
+    {
+        return [
+            'guard',
+            new Middleware('admin', except: ['logout']),
+        ];
+    }
+
+    public function index()
+    {
+
+        return view('adminauth.index', compact(['todays_sales', 'today_users', 'orders', 'subscribers', 'new_orders', 'new_users']));
+    }
+
+
+
+
 }
