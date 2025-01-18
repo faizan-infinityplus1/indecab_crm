553e4c2227019a7bb8bef585a38e71bfdfa7bb71
Faizan Baig <faizan.baig@infinityplus1.in>
2025-01-18 17:24:56 +0530

    Addded changes on show blade

--- resources/views/backend/admin/masters/dutytypes/show.blade.php
+++ resources/views/backend/admin/masters/dutytypes/show.blade.php
@@ -11,12 +11,10 @@
                     <div class="col-md-6" x-show="open">
                         <h1>New Duty Types</h1>
                     </div>
-                    <div class="col-md-6 text-end">
-                        {{-- <div class="btn-group" role="group"><a href="#" class="btn btn-primary">Add Duty Type</a></div> --}}
-                    </div>
                 </div>
             </div>
             {{-- page heading end --}}
+
             {{-- form start --}}
             <div>
                 <form>
@@ -24,22 +22,22 @@
                         <label for="" class="form-label required">Type</label>
                         <select class="form-select border-bottom" aria-label="Default select example" name=""
                             id="" @change="changedType($event.target.value)" required>
-                            <option value="1">(Select One)</option>
-                            <option value="2">HR-KM (Local)</option>
-                            <option value="3">Day-KM (Outstation)</option>
-                            <option value="4">Long Duration - Total KM Daily HR (Monthly Bookings)</option>
-                            <option value="5">Long Duration - Total KM &amp; HR (Monthly Bookings)</option>
-                            <option value="6">Flat Rate</option>
+                            <option value="selectOne">(Select One)</option>
+                            <option value="hrKmLocal">HR-KM (Local)</option>
+                            <option value="dayKmOutstation">Day-KM (Outstation)</option>
+                            <option value="longDurationDaily">Long Duration - Total KM Daily HR (Monthly Bookings)</option>
+                            <option value="longDurationMonthly">Long Duration - Total KM &amp; HR (Monthly Bookings)</option>
+                            <option value="flatRate">Flat Rate</option>
                         </select>
                         <span class="warning-msg-block"></span>
                     </div>
-                    <div class="mb-3" >
+                    <div class="mb-3">
                         <label for="" class="form-label required">Duty Type Name </label>
                         <input type="text" class="form-control  border-bottom" id="" required>
                         <span class="warning-msg-block"></span>
                     </div>
                     {{-- HR-KM (Local) --}}
-                    <div >
+                    <div>
                         <div class="mb-3" x-show="maxHours">
                             <label for="" class="form-label required">Maximum Hours </label>
                             <input type="number" class="form-control  border-bottom" id="" required>
@@ -65,6 +63,36 @@
                             </label>
                         </div>
                     </div>
+
+
+                    {{-- Long Duration - Total KM Daily HR (Monthly Bookings) --}}
+                    <div>
+                        <div class="mb-3" x-show="maxHoursPerDay">
+                            <label for="" class="form-label required">Maximum Hours per day </label>
+                            <input type="number" class="form-control  border-bottom" id="" required>
+                            <span class="warning-msg-block"></span>
+                        </div>
+                        <div class="mb-3" x-show="totalKm">
+                            <label for="" class="form-label required">Total Km </label>
+                            <input type="number" class="form-control  border-bottom" id="" required>
+                            <span class="warning-msg-block"></span>
+                        </div>
+                        <div class="mb-3" x-show="maxDays">
+                            <label for="" class="form-label required">Maximum days </label>
+                            <input type="number" class="form-control  border-bottom" id="" required>
+                            <span class="warning-msg-block"></span>
+                        </div>
+                    </div>
+
+                    {{-- Long Duration - Total KM & HR (Monthly Bookings) --}}
+                    <div>
+                        <div class="mb-3" x-show="totalHours">
+                            <label for="" class="form-label required">Total Hours</label>
+                            <input type="number" class="form-control  border-bottom" id="" required>
+                            <span class="warning-msg-block"></span>
+                        </div>
+                    </div>
+
                     {{-- Long Duration - Total KM & HR (Monthly Bookings) --}}
                     <div class="mb-3">
                         <label for="" class="form-label">Limit this duty type to be selected only for these
@@ -79,6 +107,19 @@
                             <option value="">City 6</option>
                         </select>
                         <span class="warning-msg-block"></span>
+                    </div>
+
+                    <div x-show="subType">
+                        <div class="mb-3">
+                            <label for="" class="form-label required">Sub Type</label>
+                            <select class="form-select border-bottom" aria-label="Default select example" name=""
+                                id="">
+                                <option value="">(Select One)</option>
+                                <option value="">Airport</option>
+                                <option value="">Local</option>
+                            </select>
+                            <span class="warning-msg-block"></span>
+                        </div>
                     </div>
 
                     <div class="form-check mb-3">
@@ -136,29 +177,52 @@
                 maxHoursPerDay: false,
                 totalKm: false,
                 maxDays: false,
-                totalHourse: false,
+                totalHours: false,
                 reset() {
-                    this.maxHours= false;
-                    this.maxKm= false;
-                    this.subType= false;
-                    this.maxKmPerDay= false;
-                    this.applyOutSideAllowance= false;
-                    this.maxHoursPerDay= false;
-                    this.totalKm= false;
-                    this.maxDays= false;
-                    this.totalHourse= false;
+                    this.maxHours = false;
+                    this.maxKm = false;
+                    this.subType = false;
+                    this.maxKmPerDay = false;
+                    this.applyOutSideAllowance = false;
+                    this.maxHoursPerDay = false;
+                    this.totalKm = false;
+                    this.maxDays = false;
+                    this.totalHours = false;
                 },
-                changedType(targetValue){
+                changedType(targetValue) {
                     console.log("i m called", targetValue);
                     switch (targetValue) {
-                        case "2":
+                        case "selectOne":
+                            console.log('i m called');
+                            this.selectOne();
+                            break;
+                        case "hrKmLocal":
                             console.log('i m called');
                             this.hrKmLocal();
                             break;
-                    
+                        case "dayKmOutstation":
+                            console.log('i m called');
+                            this.dayKmOutstation();
+                            break;
+                        case "longDurationDaily":
+                            console.log('i m called');
+                            this.longDurationDaily();
+                            break;
+                        case "longDurationMonthly":
+                            console.log('i m called');
+                            this.longDurationMonthly();
+                            break;
+                        case "flatRate":
+                            console.log('i m called');
+                            this.flatRate();
+                            break;
+
                         default:
                             break;
                     }
+                },
+                selectOne() {
+                    this.reset();
                 },
                 hrKmLocal() {
                     this.reset();
@@ -166,6 +230,29 @@
                     this.maxKm = true;
                     this.subType = true;
                 },
+                dayKmOutstation() {
+                    this.reset();
+                    this.maxKmPerDay = true;
+                    this.applyOutSideAllowance = true;
+                },
+                longDurationDaily() {
+                    this.reset();
+                    this.maxHoursPerDay = true;
+                    this.totalKm = true;
+                    this.maxDays = true;
+                },
+                longDurationMonthly() {
+                    this.reset();
+                    this.maxHoursPerDay = true;
+                    this.maxDays = true;
+                    this.totalHours = true;
+                },
+                flatRate(){
+                    this.reset();
+                    this.subType = true;
+                    
+                }
+
             }))
         })
     </script>
