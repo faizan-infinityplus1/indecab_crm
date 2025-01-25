f8a2b8948be1e5c65e58f8a2c93e534e480a6fc9
Faizan Baig <faizan.baig@infinityplus1.in>
2025-01-25 20:12:20 +0530

    Added migration for mst_customers

--- database/migrations/2025_01_25_135547_create_mst_customer_groups.php
+++ database/migrations/2025_01_25_135547_create_mst_customer_groups.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('mst_customers', function (Blueprint $table) {
+        Schema::create('mst_customer_groups', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('mst_customers');
+        Schema::dropIfExists('mst_customer_groups');
     }
 };
